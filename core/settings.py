7dea23f04a7821b9b1f52e9f750a1d7375c8d14e
maxXx <giulianopicco@gmail.com>
2024-09-29 17:23:31 -0400

    add 0.0.0.0 host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b#yad&8#d7j*(^40(6)*hm#cz6oys%rj8%a1i62$5s@m!zwu7m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.onrender.com', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
